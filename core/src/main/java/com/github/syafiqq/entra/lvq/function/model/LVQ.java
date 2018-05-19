c0ac0a7a853ab4764fac228a33aaac1a157431bd
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-05-19 21:20:16 +0700

    Add Abstract LVQ

---
+++ core/src/main/java/com/github/syafiqq/entra/lvq/function/model/LVQ.java
@@ -0,0 +1,45 @@
+package com.github.syafiqq.entra.lvq.function.model;
+
+import com.github.syafiqq.entra.lvq.model.database.pojo.DatasetPojo;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * This <entra-lvq> created by :
+ * Name         : syafiq
+ * Date / Time  : 19 May 2018, 5:59 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public abstract class LVQ<T>
+{
+    protected final List<ProcessedWeightPojo<T>> processed;
+    protected final List<DatasetPojo> dataset;
+
+    public LVQ()
+    {
+        this.processed = new LinkedList<>();
+        this.dataset = new LinkedList<>();
+    }
+
+    public abstract void initialization();
+
+    public abstract boolean isSatisfied();
+
+    public abstract void evaluateSatisfaction();
+
+    public abstract void reduceLearningRate();
+
+    public abstract void train();
+
+    public void run()
+    {
+        this.initialization();
+        while(!this.isSatisfied())
+        {
+            this.train();
+        }
+        this.reduceLearningRate();
+        this.evaluateSatisfaction();
+    }
+}
