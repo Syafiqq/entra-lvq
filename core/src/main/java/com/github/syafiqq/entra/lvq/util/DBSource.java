581f26a30f6d0a397f804dc2c54c6b58aceccf8f
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-05-19 16:43:23 +0700

    Add Db Source

---
+++ core/src/main/java/com/github/syafiqq/entra/lvq/util/DBSource.java
@@ -0,0 +1,14 @@
+package com.github.syafiqq.entra.lvq.util;
+
+/*
+ * This <entra-lvq> created by :
+ * Name         : syafiq
+ * Date / Time  : 19 May 2018, 3:10 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public enum DBSource
+{
+    DEFAULT,
+    JAR
+}
