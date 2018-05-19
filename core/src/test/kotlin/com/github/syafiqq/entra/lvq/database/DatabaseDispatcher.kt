b765710f0b8db54816efd21b4f921756fc6f0395
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-05-19 16:08:54 +0700

    Add databse dispatcher

---
+++ core/src/test/kotlin/com/github/syafiqq/entra/lvq/database/DatabaseDispatcher.kt
@@ -0,0 +1,15 @@
+package com.github.syafiqq.entra.lvq.database
+
+
+/*
+ * This <entra-lvq> created by : 
+ * Name         : syafiq
+ * Date / Time  : 19 May 2018, 2:05 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+interface DatabaseDispatcher
+{
+    fun up()
+    fun down()
+}
