b96165bc874e37ab4cd3768d5d55fd17465fa3e7
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-05-19 16:23:32 +0700

    Add Primary Key

--- core/src/test/kotlin/com/github/syafiqq/entra/lvq/database/CreateWeightTable.kt
+++ core/src/test/kotlin/com/github/syafiqq/entra/lvq/database/CreateWeightTable.kt
@@ -17,7 +17,7 @@ class CreateWeightTable: DatabaseDispatcher
     @Test override fun up()
     {
         val db = Settings.DB
-        db.connection.prepareStatement("CREATE TABLE IF NOT EXISTS ${WeightDao.TABLE} (  `no` INT(50) NOT NULL,  `g1` DOUBLE NOT NULL,  `g2` DOUBLE NOT NULL,  `g3` DOUBLE NOT NULL,  `g4` DOUBLE NOT NULL,  `g5` DOUBLE NOT NULL,  `g6` DOUBLE NOT NULL,  `g7` DOUBLE NOT NULL,  `g8` DOUBLE NOT NULL,  `g9` DOUBLE NOT NULL,  `g10` DOUBLE NOT NULL,  `g11` DOUBLE NOT NULL,  `g12` DOUBLE NOT NULL,  `g13` DOUBLE NOT NULL,  `g14` DOUBLE NOT NULL,  `g15` DOUBLE NOT NULL,  `g16` DOUBLE NOT NULL,  `g17` DOUBLE NOT NULL,  `g18` DOUBLE NOT NULL,  `g19` DOUBLE NOT NULL,  `g20` DOUBLE NOT NULL,  `g21` DOUBLE NOT NULL,  `target` INT(50) NOT NULL)")
+        db.connection.prepareStatement("CREATE TABLE IF NOT EXISTS ${WeightDao.TABLE} (  `no` INT(50) NOT NULL PRIMARY KEY ,  `g1` DOUBLE NOT NULL,  `g2` DOUBLE NOT NULL,  `g3` DOUBLE NOT NULL,  `g4` DOUBLE NOT NULL,  `g5` DOUBLE NOT NULL,  `g6` DOUBLE NOT NULL,  `g7` DOUBLE NOT NULL,  `g8` DOUBLE NOT NULL,  `g9` DOUBLE NOT NULL,  `g10` DOUBLE NOT NULL,  `g11` DOUBLE NOT NULL,  `g12` DOUBLE NOT NULL,  `g13` DOUBLE NOT NULL,  `g14` DOUBLE NOT NULL,  `g15` DOUBLE NOT NULL,  `g16` DOUBLE NOT NULL,  `g17` DOUBLE NOT NULL,  `g18` DOUBLE NOT NULL,  `g19` DOUBLE NOT NULL,  `g20` DOUBLE NOT NULL,  `g21` DOUBLE NOT NULL,  `target` INT(50) NOT NULL)")
                 .run {
                     execute()
                     close()
