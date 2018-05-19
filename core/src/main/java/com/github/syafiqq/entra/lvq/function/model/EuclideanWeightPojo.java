7221f3a3f2ea1be745ac6e20b74ae59445d127a9
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-05-19 21:11:32 +0700

    Add EuclideanWeightPojo

---
+++ core/src/main/java/com/github/syafiqq/entra/lvq/function/model/EuclideanWeightPojo.java
@@ -0,0 +1,24 @@
+package com.github.syafiqq.entra.lvq.function.model;
+
+import com.github.syafiqq.entra.lvq.model.database.pojo.DatasetPojo;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BinaryOperator;
+
+/*
+ * This <entra-lvq> created by :
+ * Name         : syafiq
+ * Date / Time  : 19 May 2018, 9:00 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class EuclideanWeightPojo extends ProcessedWeightPojo<Double>
+{
+    private final static BinaryOperator<Double> adder = (n1, n2) -> n1 + n2;
+
+    @Override public void calculateDistance(DatasetPojo pojo)
+    {
+        AtomicReference<Double> distance = new AtomicReference<>((double) 0);
+        pojo.vector.forEach((idx, val) -> distance.accumulateAndGet(Math.pow(super.vector(idx) - val, 2), adder));
+        super.distance = Math.sqrt(distance.get());
+    }
+}
