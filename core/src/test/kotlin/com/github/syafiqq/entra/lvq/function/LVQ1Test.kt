83f25823438d0fe3ae920cf725bfe121134a74c8
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-05-19 22:13:07 +0700

    Add LVQ1Test

---
+++ core/src/test/kotlin/com/github/syafiqq/entra/lvq/function/LVQ1Test.kt
@@ -0,0 +1,32 @@
+package com.github.syafiqq.entra.lvq.function
+
+import org.junit.Assert
+import org.junit.Test
+
+
+/*
+ * This <entra-lvq> created by : 
+ * Name         : syafiq
+ * Date / Time  : 19 May 2018, 10:09 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+class LVQ1Test
+{
+    @Test
+    fun `it_should_instantiate_object_successfully`()
+    {
+        val lvq = LVQ1()
+        Assert.assertNotNull(lvq)
+    }
+
+    @Test
+    fun `it_should_have_non_empty_data_after_initialize`()
+    {
+        val lvq = LVQ1()
+        lvq.initialization()
+        Assert.assertNotNull(lvq)
+        Assert.assertTrue(lvq.dataset.size > 0)
+        Assert.assertTrue(lvq.weight.size > 0)
+    }
+}
