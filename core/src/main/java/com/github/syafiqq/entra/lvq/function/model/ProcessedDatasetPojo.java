b1a4b0c2e4c45b17295b6aa49c391039d9165e2a
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-05-20 12:09:15 +0700

    Add Processed dataset Pojo

---
+++ core/src/main/java/com/github/syafiqq/entra/lvq/function/model/ProcessedDatasetPojo.java
@@ -0,0 +1,31 @@
+package com.github.syafiqq.entra.lvq.function.model;
+
+import com.github.syafiqq.entra.lvq.model.database.pojo.DatasetPojo;
+
+/*
+ * This <entra-lvq> created by :
+ * Name         : syafiq
+ * Date / Time  : 20 May 2018, 12:03 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class ProcessedDatasetPojo
+{
+    public DatasetPojo dataset;
+    public int actualTarget;
+
+    public ProcessedDatasetPojo(DatasetPojo pojo)
+    {
+        this.dataset = pojo;
+    }
+
+    public boolean isSameClass()
+    {
+        return this.dataset.target == this.actualTarget;
+    }
+
+    @Override public String toString()
+    {
+        return this.dataset.toString();
+    }
+}
