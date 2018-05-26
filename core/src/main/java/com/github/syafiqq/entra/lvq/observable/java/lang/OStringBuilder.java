06699011231db8d7a8cb673f9031559fb002c7bb
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-05-26 08:01:59 +0700

    Mark builder as public

--- core/src/main/java/com/github/syafiqq/entra/lvq/observable/java/lang/OStringBuilder.java
+++ core/src/main/java/com/github/syafiqq/entra/lvq/observable/java/lang/OStringBuilder.java
@@ -11,7 +11,7 @@ import java.util.Observable;
  */
 public class OStringBuilder extends Observable
 {
-    final StringBuilder builder;
+    public final StringBuilder builder;
 
     public OStringBuilder(StringBuilder builder)
     {
