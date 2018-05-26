b3d7d6115b01609bf9032a2f3b68f0ea5db76360
Syafiqq <id.muhammad.syafiq@gmail.com>
2018-05-26 07:22:52 +0700

    Add OStringBuilder

---
+++ core/src/main/java/com/github/syafiqq/entra/lvq/observable/java/lang/OStringBuilder.java
@@ -0,0 +1,35 @@
+package com.github.syafiqq.entra.lvq.observable.java.lang;
+
+import java.util.Observable;
+
+/*
+ * This <entra-lvq> created by :
+ * Name         : syafiq
+ * Date / Time  : 26 May 2018, 7:15 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+public class OStringBuilder extends Observable
+{
+    final StringBuilder builder;
+
+    public OStringBuilder(StringBuilder builder)
+    {
+        this.builder = builder;
+    }
+
+    public StringBuilder append(String str)
+    {
+        StringBuilder result = builder.append(str);
+        this.setChanged();
+        this.notifyObservers(result);
+        return result;
+    }
+
+    public void setLength(int newLength)
+    {
+        builder.setLength(newLength);
+        this.setChanged();
+        this.notifyObservers(this.builder);
+    }
+}
